f8cd56608b5f8df03d027a8b59a8c0a882da0037
Oscar Morales <oscar.s.morales.z@usp.br>
2022-06-15 12:10:30 -0500

    Modify SocialNetwork.py

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -1,10 +1,54 @@
+# Mensaje de bienvenida al usuario
 print("Bienvenido a Mi red social... ")
 print("""
-  ______                                                                _     
- / _____)                                         _                    | |    
-| /      ____  ____ ____ ____ ____    ____   ____| |_ _ _ _  ___   ____| |  _ 
-| |     / _  |/ ___) _  ) ___) _  |  |  _ \ / _  )  _) | | |/ _ \ / ___) | / )
-| \____( ( | | |  ( (/ ( (__( ( | |  | | | ( (/ /| |_| | | | |_| | |   | |< ( 
- \______)_||_|_|   \____)____)_||_|  |_| |_|\____)\___)____|\___/|_|   |_| \_)
+  ____ ____  ____ ____ ____ ____ 
+ / ___) _  |/ ___) _  ) ___) _  |
+( (__( ( | | |  ( (/ ( (__( ( | |
+ \____)_||_|_|   \____)____)_||_|
+                                   _          
+             _                    | |         
+ ____   ____| |_ _ _ _  ___   ____| |  _  ___ 
+|  _ \ / _  )  _) | | |/ _ \ / ___) | / )/___)
+| | | ( (/ /| |_| | | | |_| | |   | |< (|___ |
+|_| |_|\____)\___)____|\___/|_|   |_| \_|___/
 
 """)
+
+# Solicitamos al usuario que ingrese su nombre, y lo guardamos en una variable de tipo str
+name = input("Dinos como te llamas: ")
+print()
+print("Hola ", name, ", bienvenido a Careca Networks")
+print()
+
+# Solicitamos el ingreso del año, y utilizamos este dato para estimar la edad de la persona. 
+year = int(input("Para preparar tu perfil, dime en que año naciste. "))
+age = 2022-year
+print()
+
+# Solicitamos la estatura, medida en metros. Utilizamos la conversión a 'int', y una expresión para convertir esto a una medida en metros y centí­metros
+stature = float(input("Cuentame más de ti, para agregarlo a tu perfil. ¿Cuánto mides en metros?. "))
+stature_m = float(stature)
+stature_cm = int(stature_m*100 )
+print()
+
+# Consultamos cuántos amigos tiene el usuario.
+num_friends = int(input("Muy bien. Finalmente, cuéntame ¿cuantos amigos tienes?. "))
+
+# Con los datos recolectados escribimos en pantalla un texto que resuma los datos que hemos obtenido
+print()
+print("Muy bien,", name, ". Entonces podemos crear un perfil con estos datos.")
+print("--------------------------------------------------")
+print("Nombre:  ", name)
+print("Edad:    ", age, "años")
+print("Estatura:", stature_m, "metros o", stature_cm, "centímetros")
+print("Amigos:  ", num_friends)
+print("--------------------------------------------------")
+print("Gracias por la información. Esperamos que disfrutes de nuestra red")
+print()
+
+# Solicitamos un mensaje de prueba que sirva para publicar un estado del usuario.
+message = input("Ahora vamos a publicar tu primer mensaje. ¿Qué piensas hoy? ")
+print()
+print("--------------------------------------------------")
+print(name, "dice:", message)
+print("--------------------------------------------------")

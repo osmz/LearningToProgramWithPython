eb6a9004b6588ff8dfcd6e1c4ea9bdbcef569afd
Oscar Morales <oscar.s.morales.z@usp.br>
2022-06-29 08:12:50 -0500

    Modify SocialNetworks3.py

--- SocialNetwork3.py
+++ SocialNetwork3.py
@@ -52,12 +52,12 @@ def option_Menu():
     print("  0. Salir")
     option = int(input("Ingresa una opción: "))
     while option < 0 or option > 4:
-        print("No conozco la opción que has ingresado. Inténtalo otra vez.")
+        print("No conozco la opción que has ingresado. Inténtalo otra vez: ")
         option = int(input("Ingresa una opción: "))
     return option
 
-def get_Menssage():
-    message = input("Ahora vamos a publicar un mensaje. ¿Qué piensas hoy? ")
+def get_Message():
+    message = input("Ahora vamos a publicar un mensaje. ¿Qué piensas hoy?: ")
     return message
 
 def show_Message(origin, addressee, message):
@@ -83,3 +83,37 @@ print("Muy bien,", name, ". Entonces podemos crear un perfil con estos datos: ")
 show_Profile(name, age, stature_m, stature_cm, num_friends)
 print("Ya podemos preguntar, recordar y calcular datos. Esperamos que disfrutes con Mi Red. ")
 print("--------------------------------------------------")
+
+# Ingresamos al ciclo que permite ejecutar acciones
+option = 1
+while option != 0:
+    # Mostramos el menu
+    option = option_Menu()
+
+    # Código para la opción 1. Publicar un mensaje.
+    if option == 1:
+        message = get_Message()
+        show_Message(name, None, message)
+
+    # Código para la opción 2. Publicar un mensaje a un grupo de personas.
+    elif option == 2:
+        message = get_Message()
+        for i in range(num_friends):
+            name_friends = input("Ingresa el nombre de tu amigo o amiga: ")
+            show_Message(name, name_friends, message)
+
+    # Código para la opción 3. Publicar los datos del perfil del usuario.
+    elif option == 3:
+        show_Profile(name, age, stature_m, stature_cm, num_friends)
+
+    # Código para la opción 4. Actualizar los datos del perfil del usuario.
+    elif option == 4:
+        name = get_Name()
+        age = get_Age()
+        (stature_m, stature_cm) = get_Stature()
+        num_friends = get_Num_Friends()
+        show_Profile(name, age, stature_m, stature_cm, num_friends)
+    elif option == 0:
+        print("Has decidido salir.")
+
+print("Gracias por usar Mi Red. ¡Hasta pronto!")
